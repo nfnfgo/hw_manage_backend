c340e138a6ddb72750e0ead8fd685c545d52b824
Oyasuminasai <61616918+nfnfgo@users.noreply.github.com>
2022-09-11 23:01:08 +0800

    Save

--- api/user.py
+++ api/user.py
@@ -1,6 +1,7 @@
 '''Handle /user request'''
 
 
+from ast import expr_context
 import asyncio
 import json
 from operator import truediv
@@ -17,12 +18,12 @@ user_route = web.RouteTableDef()
 # Receive Post request
 @user_route.post(config.api_url_suffix + '/user/list')
 async def user_list(request: web.Request):
-    headers = {'Access-Control-Allow-Origin':'*'}
+    '''Deal with User List Acquaintance Request'''
     try:
         print(await request.json())
         req = await request.json()
     except:
-        return web.Response(text='Valid Json Body', status=400,headers=config.headers)
+        return web.Response(text='Invalid Json Body', status=400, headers=config.headers)
     try:
         apikey = req['apikey']
     except:
@@ -32,4 +33,47 @@ async def user_list(request: web.Request):
     if api_check_res != True:
         return web.Response(text=api_check_res, status=401)
     result = await users.get_user_list()
-    return web.Response(content_type='application/json', text=json.dumps(result),headers=config.headers)
+    return web.Response(content_type='application/json', text=json.dumps(result), headers=config.headers)
+
+
+@user_route.post(config.api_url_suffix+'/user/point')
+async def user_point(request: web.Request):
+    '''Deal with requests about users point, such as changing point ans so on'''
+    # if the input body can't be parsed as json, return error
+    try:
+        req = await request.json()
+    except:
+        return web.Response(text='Invalid Json Body', status=400, headers=config.headers)
+    # Auth Key (opt about point is sensitive)
+    try:
+        apikey = req['apikey']
+    except:
+        apikey = None
+    api_check_res = await key_checker.check(apikey)
+    if api_check_res != True:
+        return web.Response(text=api_check_res, status=401)
+
+    # Start to respones with the request
+    try:
+        api_method = req['api_method']
+    except:
+        api_method = None
+
+    # Deal with different API Method
+    # Change Point Method
+    if api_method == 'change_point':
+        # read param
+        try:
+            user_id = req['user_id']
+            d_point = req['d_point']
+        except:
+            return web.Response(text='Params Missing: user_id or d_point', status=400, headers=config.headers)
+        # if params ok, start to retrive data from mySQL
+        try:
+            
+        except:
+            pass
+        return web.Response(text='Request Success, but API is Developing...', status=404)
+
+    # If no specified api_method
+    return web.Response(text='No Specified api_method: change_point or ...', status=404)
--- sql/users.py
+++ sql/users.py
@@ -22,6 +22,43 @@ async def get_user_list() -> list | str:
         async with pool.acquire() as conn:
             conn: aiomysql.Connection
             async with conn.cursor() as cursor:
+                await conn.commit()
+                cursor: aiomysql.Cursor
+                await cursor.execute('''SELECT * FROM users ORDER by point DESC''')
+                res = await cursor.fetchall()
+    except:
+        return 'Internal Error: Cannot connected to SQL Server'
+    # If data got succesfully, start to format the info
+    user_list = []
+    # NEED REWRITE
+    # use specified mark but NOT Sequence
+    for single_user in res:
+        single_user_dict = {}
+        single_user_dict['id'] = single_user[0]
+        single_user_dict['name'] = single_user[1]
+        single_user_dict['status'] = single_user[2]
+        single_user_dict['point'] = single_user[3]
+        user_list.append(single_user_dict)
+    return user_list
+
+
+async def change_user_point() -> dict | str:
+    '''(Asynchronous) Change the user point
+    
+    Params:
+    user_id: Unique ID of user
+    d_point: Means delta_point.
+
+    Returns:
+    dict_info: Dict type.
+    '''
+    # Get a conn pool
+    try:
+        pool = await SQLPool()
+        async with pool.acquire() as conn:
+            conn: aiomysql.Connection
+            async with conn.cursor() as cursor:
+                await conn.commit()
                 cursor: aiomysql.Cursor
                 await cursor.execute('''SELECT * FROM users ORDER by point DESC''')
                 res = await cursor.fetchall()
